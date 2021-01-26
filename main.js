cff12bcd6022cb76f234ee23f6623357a6364d9f
NINJA-Ai <68237917+NINJA-Ai@users.noreply.github.com>
2021-01-26 23:58:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
-var canvas = new fabic.Canvas('myCanvas');
-player_x = 10;
-player_y = 10;
-
-block_image_width = 30;
-block_image_height = 30;
+var canvas = new fabric.Canvas('myCanvas');
+var block_image_width = 30;
+var block_image_height = 30;
+var player_x = 10;
+var player_y = 10;
 
 var player_object = "";
 var block_image_object = "";
 
-function player_update();
+updateWidthHeight();
+
+function updateWidthHeight()
 {
-    fabic.Image.fromURL("player.png", function(Img){
-player_object=Img;
-player_object.scaleToWidth(150);
-player_object.scaleToHeight(160);
-player_object.set({
-    top:player_y,left:player_x
-});
-canvas.add(player_object);
-});
+    console.log("Width= " + block_image_width + " Height= " + block_image_height);
+    document.getElementById('current_width').innerHTML = block_image_width;
+    document.getElementById('current_height').innerHTML = block_image_height;
 }
+
+function player_update()
+{
+    fabic.Image.fromURL("player.png", 
+        function(Img)
+        {
+            player_object=Img;
+            player_object.scaleToWidth(150);
+            player_object.scaleToHeight(160);
+            player_object.set({
+                top:player_y,left:player_x
+            });
+            canvas.add(player_object);
+        }
+    );
+}
+
 function new_image(get_img)
 {
-    fabic.Image.fromURL(get_image, function(Img){
-        player_object=Img;
-        player_object.scaleToWidth(150);
-        player_object.scaleToHeight(160);
-        player_object.set({
-            top:player_y,left:player_x
-        });
-        canvas.add(player_object);
-        });
+    fabic.Image.fromURL(get_image, 
+        function(Img){
+            player_object=Img;
+            player_object.scaleToWidth(150);
+            player_object.scaleToHeight(160);
+            player_object.set({
+                top:player_y,left:player_x
+            });
+            canvas.add(player_object);
+        }
+    );
 }
-
-
-
-
-
-
